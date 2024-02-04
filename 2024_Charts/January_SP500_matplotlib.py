9aed6c3d36b1882bed2f7ef03c62afebdd29523c
Quant Girl <quantgirl.uk@gmail.com>
2024-02-04 12:31:51 +0000

    Update January_SP500_matplotlib.py

--- 2024_Charts/January_SP500_matplotlib.py
+++ 2024_Charts/January_SP500_matplotlib.py
@@ -6,7 +6,7 @@ import yfinance as yf
 quant_pastel = "https://raw.githubusercontent.com/quantgirluk/matplotlib-stylesheets/main/quant-pastel-light.mplstyle"
 plt.style.use(quant_pastel)
 
-palette = px.colors.qualitative.Pastel
+palette = px.colors.qualitative.Bold
 colors = [(a / 255, b / 255, c / 255) for (a, b, c) in [eval(p[4:-1]) for p in palette]]
 colors_transparent1 = [(a / 255, b / 255, c / 255, 0.7) for (a, b, c) in [eval(p[4:-1]) for p in palette]]
 colors_transparent2 = [(a / 255, b / 255, c / 255, 0.15) for (a, b, c) in [eval(p[4:-1]) for p in palette]]
