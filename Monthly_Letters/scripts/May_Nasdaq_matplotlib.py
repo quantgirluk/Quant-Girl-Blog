a420a5e9c0cc5c494a65f54ad40ce2228552becf
Quant Girl <quantgirl.uk@gmail.com>
2024-06-03 12:01:32 +0100

    Updated Monthly charts

--- Monthly_Letters/scripts/May_Nasdaq_matplotlib.py
+++ Monthly_Letters/scripts/May_Nasdaq_matplotlib.py
@@ -7,40 +7,52 @@ plt.style.use(quant_pastel)
 
 plt.rc('font', family='sans-serif')
 plt.rc('font', serif='Apple SD Gothic Neo')
-plt.rcParams.update({'font.size': 14})
+# plt.rcParams.update({'font.size': 14})
 
-data = yf.download('^IXIC')
+#  Index Chart
 
 
+# Create the figure and axes object
+fig, ax = plt.subplots(figsize=(12., 7.5), dpi=100)
 
-ax = data['Close'].plot.area(figsize=(12, 6), alpha=0.25)
+data_index = yf.download('^IXIC')
+data_index['Close'].plot.area(alpha=0.25, ax=ax)
 
-ax.set_title("Nasdaq Composite Index - Historical Close Levels\n", fontsize=16)
-ax.set_xlabel('Date')
+ax.set_xlabel('')
 ax.set_ylabel(r'Points')
 
 # Add in red line and rectangle on top
-ax.plot([0.0, .98], [1., 1.], transform=ax.transAxes, clip_on=False, color='#E3120B', linewidth=.6)
-ax.add_patch(plt.Rectangle((0.0,1.), 0.04, -0.02, facecolor='#E3120B', transform=ax.transAxes, clip_on=False, linewidth = 0))
+ax.plot([0.05, .95], [.98, .98], transform=fig.transFigure, clip_on=False, color='#E3120B', linewidth=.6)
+ax.add_patch(plt.Rectangle((0.05, .98), 0.04, -0.02, facecolor='#E3120B', transform=fig.transFigure,
+                           clip_on=False, linewidth=0))
+
+# Add in title and subtitle
+ax.text(x=0.05, y=.93, s="Nasdaq Composite Index", transform=fig.transFigure, ha='left', fontsize=14, weight='bold',
+        alpha=.9)
+ax.text(x=0.05, y=.90, s="Historical Close Levels from 1971 to 2024", transform=fig.transFigure, ha='left', fontsize=12,
+        alpha=.9)
+
+# Set source text
+ax.text(x=0.05, y=0.075, s="Source: Yahoo Finance", transform=fig.transFigure, ha='left', fontsize=10, alpha=.8)
+
+# Adjust the margins around the plot area
+plt.subplots_adjust(left=None, bottom=0.2, right=None, top=0.85, wspace=None, hspace=None)
 
 # format the ticks only for years on the major ticks
-years = mdates.YearLocator(5)   # every 5 year
+years = mdates.YearLocator(5)  # every 5 year
 years_fmt = mdates.DateFormatter('%Y')
 
 ax.xaxis.set_major_locator(years)
 ax.xaxis.set_major_formatter(years_fmt)
+ax.xaxis.set_tick_params(pad=2, labelbottom=True, bottom=True, labelsize=12, labelrotation=0)
 
 ax.set_ylim(0, 18000)
-ax.yaxis.tick_right()
-ax.yaxis.set_label_position("right")
-
-ax.xaxis.grid() # horizontal lines
+ax.xaxis.grid()  # horizontal lines
 ax.set_axisbelow(True)
 
-ax.spines[['top']].set_visible(False)
-ax.spines[['right', 'bottom']].set_visible(True)
+ax.spines[['right', 'top']].set_visible(False)
+ax.spines[['bottom']].set_visible(True)
 
-# matplotlib.patch.Patch properties
 props = dict(boxstyle='round, pad=1', facecolor='oldlace', edgecolor='cornsilk', alpha=0.95)
 
 my_text = '\n'.join((r'The Nasdaq Composite is a stock market index that includes almost ',
@@ -51,15 +63,16 @@ my_text = '\n'.join((r'The Nasdaq Composite is a stock market index that include
                      r'indices in the United States. ',
                      r'On May 28, 2024 the index closed above the 17,000 points for the ',
                      r'first time in history. '
-                    ))
+                     ))
 
 ax.text(0.025, 0.9, my_text, transform=ax.transAxes, fontsize=12,
         verticalalignment='top', bbox=props)
 
 plt.tight_layout()
-ax.figure.savefig("../charts/05_may_nasdaq", dpi=300)
+ax.figure.savefig("../charts/05_may_nasdaq", dpi=200)
 plt.show()
 
+# 5 Largest Components Nasdaq
 
 tickers = ['MSFT', 'AAPL', 'NVDA', 'AMZN', 'META']
 data = yf.download(tickers, start='2014-01-01', )
@@ -70,8 +83,8 @@ highs = [('Close', 'MSFT'),
          ('Close', 'META'),
          ]
 
-ax = data[highs].plot(figsize=(12, 8))
-ax.set_title(label="Nasdaq Composite Largest Components - Historical Close Prices", fontsize=16)
+fig, ax = plt.subplots(figsize=(12., 7.5), dpi=100)
+data[highs].plot(figsize=(12, 8), ax=ax)
 ax.set_xlabel('Date')
 ax.set_ylabel(r'Price (\$)')
 
@@ -83,12 +96,31 @@ ax.set_axisbelow(True)
 ax.spines[['top']].set_visible(False)
 ax.spines[['right', 'bottom']].set_visible(True)
 
- # Add in red line and rectangle on top
-ax.plot([0.0, .98], [1., 1.], transform=ax.transAxes, clip_on=False, color='#E3120B', linewidth=.6)
-ax.add_patch(plt.Rectangle((0.0,1.), 0.04, -0.02, facecolor='#E3120B', transform=ax.transAxes, clip_on=False, linewidth = 0))
+years = mdates.YearLocator(2)  # every 2 year
+years_fmt = mdates.DateFormatter('%Y')
 
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.xaxis.set_tick_params(pad=2, labelbottom=True, bottom=True, labelsize=12, labelrotation=0)
+
+# Add in red line and rectangle on top
+ax.plot([0.05, .95], [.98, .98], transform=fig.transFigure, clip_on=False, color='#E3120B', linewidth=.6)
+ax.add_patch(plt.Rectangle((0.05, .98), 0.04, -0.02, facecolor='#E3120B', transform=fig.transFigure,
+                           clip_on=False, linewidth=0))
+
+# Add in title and subtitle
+ax.text(x=0.05, y=.93, s="Nasdaq Composite 5 Largest Components", transform=fig.transFigure, ha='left', fontsize=14,
+        weight='bold', alpha=.9)
+ax.text(x=0.05, y=.90, s="Historical Close Prices from 2014 to 2024", transform=fig.transFigure, ha='left', fontsize=12,
+        alpha=.9)
+
+# Set source text
+ax.text(x=0.05, y=0.075, s="Source: Yahoo Finance", transform=fig.transFigure, ha='left', fontsize=10, alpha=.8)
+
+# Adjust the margins around the plot area
+plt.subplots_adjust(left=None, bottom=0.2, right=None, top=0.85, wspace=None, hspace=None)
 
 plt.tight_layout()
-ax.figure.savefig("../charts/05_may_nasdaq_largest_components", dpi=300)
+ax.figure.savefig("../charts/05_may_nasdaq_largest_components", dpi=200)
 
 plt.show()
