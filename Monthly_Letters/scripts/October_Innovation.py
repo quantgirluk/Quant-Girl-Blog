663793ac36a4a4e61c0203b05b78a586d6f67006
Quant Girl <quantgirl.uk@gmail.com>
2024-10-31 22:12:56 +0000

    Update October_Innovation.py

--- Monthly_Letters/scripts/October_Innovation.py
+++ Monthly_Letters/scripts/October_Innovation.py
@@ -1,31 +1,9 @@
+import chart_studio.plotly as py
 import pandas as pd
 import plotly.express as px
 
 data = pd.read_csv('../data/wipo_data.csv')
-# df = data
 df = data.sort_values(by='GDP per capita (PPP$ logarithmic scale)')
-
-
-
-# df['x'] = df['GDP per capita (PPP$ logarithmic scale)']
-# df['y'] = df['GII score']
-#
-#
-#
-# import statsmodels.formula.api as smf
-#
-# model = smf.ols(formula='y ~ I(x**3 + x**2 + x)', data=df).fit()
-# predicted = model.predict(df.x)
-#
-# from scipy.interpolate import CubicSpline
-#
-# d = df[['x', 'SPLINE']].drop_duplicates(['x'], keep='first')
-#
-#
-# # cs = CubicSpline(d['x'], d['y'])
-#
-# import numpy as np
-# xs = np.arange(7, 12, 0.1)
 
 fig = px.scatter(df, x='GDP per capita (PPP$ logarithmic scale)', y='GII score',
                  size='POP',
@@ -35,12 +13,9 @@ fig = px.scatter(df, x='GDP per capita (PPP$ logarithmic scale)', y='GII score',
                  hover_name='NAME',
                  color_discrete_sequence=px.colors.qualitative.Bold,
                  template='plotly_white',
-                 title='Innovation relative to economic development')
+                 title='Innovation relative to economic development <br>Global Innovation Index 2024')
 
-# fig = fig.add_scatter(x=d['x'],
-#                 y=d['SPLINE'], name='Cubic Spline')
-
-# fig.add_scatter(x=d.x, y=predicted, name="Fitted Polynomial")
-# fig.add_scatter(x=xs, y=cs(xs), name="Cubic Spline Fitted Polynomial")
-
+PUBLISH = True
+if PUBLISH:
+    py.plot(fig, filename='October_2024_Innovation_Plotly', auto_open=True)
 fig.show()
